d4986728b736765537f7c33802cabcd3398edb3c
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-21 20:55:35 -0300

    box listener

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ let inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function(){
     console.log("Button cliked from addEventListener")
 })
+
+let boxi = document.getElementById("box")
+
+boxi.addEventListener("click", function(){
+    console.log("I want to open the box!")
+})
