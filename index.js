a8b67e10bb50397850323938b9dc1e8c2c950d1b
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-22 17:08:43 -0300

    render text content

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -9,5 +9,6 @@ inputBtn.addEventListener("click", function(){
 })
 
 for(let i = 0; i < myLeads.length; i++){
-    console.log(myLeads[i])
+    ulEl.textContent += myLeads[i] + " "
+
 }
