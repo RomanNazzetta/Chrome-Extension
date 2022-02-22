88bb78f94b134f57253581fc4ede7c39953bdc00
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-21 21:08:56 -0300

    Const input

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el ")
+const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function(){
     console.log("Button cliked from addEventListener")
-})
-
-let boxi = document.getElementById("box")
-
-boxi.addEventListener("click", function(){
-    console.log("I want to open the box!")
 })
