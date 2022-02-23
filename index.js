e6835648fa2f8af251deaab02ddedb31b958195d
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-23 18:23:59 -0300

    delete-btn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-console.log(leadsFromLocalStorage)
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
