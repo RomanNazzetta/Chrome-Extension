889e4a89ea44c9bd3fee7db0f6e0a908517117fd
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-24 19:23:20 -0300

    permision JSON

--- index.js
+++ index.js
@@ -12,16 +12,13 @@ if(leadsFromLocalStorage){
     render(myLeads)
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/roman-nazzetta-38173a1b7/"}
-]
-
 
 tabBtn.addEventListener("click", function(){
-    //console.log(tabs[0].url)
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })    
 })
 
 
