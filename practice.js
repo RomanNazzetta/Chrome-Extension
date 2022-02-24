a3ecf02096a112e5c683c110d90cf58bf5ac42c2
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-24 20:02:17 -0300

    log out items

--- practice.js
+++ practice.js
@@ -1,16 +1,28 @@
-const player = "Roman"
-const opponent = "Flor"
-const game = "AmazingFighter"
+// const player = "Roman"
+// const opponent = "Flor"
+// const game = "AmazingFighter"
 
-let points = 0
-let hasWon = false
+// let points = 0
+// let hasWon = false
 
-points += 100
-hasWon = true
+// points += 100
+// hasWon = true
 
 
-if (hasWon) {
-    console.log(`${player} got ${points} points and won the ${game} game!`)
-}else {
-    console.log( `The winner is ${opponent}! ${player} lost the game`)
+// if (hasWon) {
+//     console.log(`${player} got ${points} points and won the ${game} game!`)
+// }else {
+//     console.log( `The winner is ${opponent}! ${player} lost the game`)
+// }
+
+
+let myCourses = ["Learn Css Animations", "UI Design Fundamentals", "Intro to Clean Code"]
+
+
+function logItems(arr){
+    for (let i = 0; i < arr.length; i++){
+        console.log(arr[i])
+    }
 }
+
+logItems(myCourses)
